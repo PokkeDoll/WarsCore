7a07a3d86329c04286a7e091fc0f7af63c8d9863
PokkeDoll <pokkedoll@gmail.com>
2020-10-24 14:43:33 +0900

    0.36.1

--- src/main/scala/hm/moe/pokkedoll/warscore/commands/InviteCommand.scala
+++ src/main/scala/hm/moe/pokkedoll/warscore/commands/InviteCommand.scala
@@ -1,9 +1,12 @@
 package hm.moe.pokkedoll.warscore.commands
 
-import hm.moe.pokkedoll.warscore.WarsCoreAPI
+import java.util.UUID
+
+import hm.moe.pokkedoll.warscore.{WarsCore, WarsCoreAPI}
+import io.chazza.advancementapi.{AdvancementAPI, FrameType}
 import net.md_5.bungee.api.ChatColor
 import net.md_5.bungee.api.chat.{ClickEvent, ComponentBuilder}
-import org.bukkit.Bukkit
+import org.bukkit.{Bukkit, NamespacedKey, Sound}
 import org.bukkit.command.{Command, CommandExecutor, CommandSender}
 import org.bukkit.entity.Player
 
@@ -17,25 +20,46 @@ class InviteCommand extends CommandExecutor {
         if(args.length == 0) {
           player.sendMessage("§c使用方法: /invite §a<プレイヤー名>")
         } else {
-          Option(Bukkit.getPlayer(args(0))) match {
+          val name = args(0)
+          Option(Bukkit.getPlayer(name)) match {
             case Some(target) =>
               val wp = WarsCoreAPI.getWPlayer(player)
               val tp = WarsCoreAPI.getWPlayer(target)
+              wp.game match {
+                case Some(_) if tp.game.isDefined =>
+                  player.sendMessage(ChatColor.GREEN + name + ChatColor.RED + s"は他のゲーム ${tp.game.get.title}(id: ${tp.game.get.id}に参加しています！")
+                case Some(game) =>
+                  target.sendMessage(
+                    new ComponentBuilder("= = = = = = = = = = = = = = = = = = = = =\n")
+                      .append(s"${player.getName}").color(ChatColor.GREEN).append("から招待が届きました！").color(ChatColor.WHITE)
+                      .append("ゲームモード: ").color(ChatColor.YELLOW).append(wp.game.get.title).color(ChatColor.GREEN).append("\n")
+                      .append("参加するにはこのメッセージをクリック！！").bold(true).color(ChatColor.AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, s"/game join ${game.id}"))
+                      .append("\n= = = = = = = = = = = = = = = = = = = = =").color(ChatColor.WHITE)
+                      .create():_*
+                  )
+                  val advancement = AdvancementAPI.builder(new NamespacedKey(WarsCore.instance, "story/" + UUID.randomUUID().toString))
+                    .frame(FrameType.TASK)
+                    .icon("minecraft:bow")
+                    .title(s"§a${player.getName}§fからゲームに招待されました！チャット欄を確認してください！")
+                    .description("description")
+                    .background("minecraft:textures/blocks/bedrock.png")
+                    .announce(false)
+                    .toast(true)
+                    .build()
+                  advancement.show(WarsCore.instance, target)
+                  target.playSound(target.getLocation, Sound.ENTITY_VILLAGER_YES, 1f, 1f)
+                case None =>
+                  player.sendMessage(ChatColor.RED + "ゲームに参加していないため招待できませんでした")
+              }
               if(wp.game.isEmpty) {
                 player.sendMessage("§cゲームに参加していないため招待できませんでした")
               } else if (tp.game.isDefined) {
                 player.sendMessage(s"§a${target.getName}§cはすでにゲーム(id: ${tp.game.get.id}に参加しています！")
               } else {
-                player.sendMessage(
-                  new ComponentBuilder("= = = = = = = = = = = = = = = = = = = = =\n")
-                    .append(s"${player.getName}").color(ChatColor.GREEN).append("から招待が届きました！").color(ChatColor.WHITE)
-                    .append("ゲームモード: ").color(ChatColor.YELLOW).append(wp.game.get.title).color(ChatColor.GREEN).append("\n")
-                    .append("参加するにはこのメッセージをクリック！！").bold(true).color(ChatColor.AQUA).event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, s"/game join ${wp.game.get.id}"))
-                    .create():_*
-                )
+
               }
             case None =>
-              player.sendMessage("")
+              player.sendMessage(ChatColor.RED + s"$name は存在しません！")
           }
         }
       case _ =>
