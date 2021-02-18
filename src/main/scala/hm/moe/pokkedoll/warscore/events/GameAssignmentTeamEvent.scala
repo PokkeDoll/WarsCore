c62090aa082a8987a866faf92ba16523000d3f6c
PokkeDoll <pokkedoll@gmail.com>
2021-02-18 20:04:13 +0900

    update: get player when call this event

--- src/main/scala/hm/moe/pokkedoll/warscore/events/GameAssignmentTeamEvent.scala
+++ src/main/scala/hm/moe/pokkedoll/warscore/events/GameAssignmentTeamEvent.scala
@@ -1,15 +1,24 @@
 package hm.moe.pokkedoll.warscore.events
 
-import hm.moe.pokkedoll.warscore.games.{Game, GameTeam}
+import hm.moe.pokkedoll.warscore.games.Game
 import org.bukkit.entity.Player
 import org.bukkit.event.{Event, HandlerList}
 
 import scala.collection.mutable
 
-
-class GameAssignmentTeamEvent[T <: {def getTeam: GameTeam}](private val game: Game, private var data: mutable.Map[Player, T]) extends Event {
+/**
+ * チーム決めがされた時に呼ばれるイベント
+ *
+ * @param game   ゲーム
+ * @param players 参加したプレイヤー
+ * @param data   データ
+ * @tparam T GamePlayerData
+ */
+class GameAssignmentTeamEvent[T](private val game: Game, private val players: Array[Player], private var data: mutable.Map[Player, T]) extends Event {
 
   def getGame: Game = game
+
+  def getPlayers: Array[Player] = players
 
   def getData: mutable.Map[Player, T] = data
 
