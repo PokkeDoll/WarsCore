05824c1770390e71189c6cc5d816645e6a4305f7
PokkeDoll <pokkedoll@gmail.com>
2021-02-28 01:12:43 +0900

    update: change icon

--- src/main/scala/hm/moe/pokkedoll/warscore/ui/GameUI.scala
+++ src/main/scala/hm/moe/pokkedoll/warscore/ui/GameUI.scala
@@ -22,29 +22,53 @@ object GameUI {
   private def createIcon(game: Game): ItemStack = {
     val i = new ItemStack(Material.STONE)
     val m = i.getItemMeta
-    if(game.state == GameState.PLAY || game.state == GameState.WAIT || game.state == GameState.READY) {
-      i.setType(Material.LIME_DYE)
-      i.setAmount(if(game.members.isEmpty) 1 else game.members.size)
-      m.setDisplayName(ChatColor.GREEN + game.id)
-      m.setLore(java.util.Arrays.asList(
-        ChatColor.LIGHT_PURPLE + game.title,
-        ChatColor.GREEN + game.mapInfo.mapName,
-        ChatColor.GREEN + (if(game.state == GameState.PLAY) "試合中！" else if (game.state == GameState.WAIT) s"${game.members.size} / ${game.maxMember} 待機中！" else s"${game.members.size} / ${game.maxMember} 準備中！"),
-      ))
-    } else if (game.state == GameState.INIT) {
-      i.setType(Material.PINK_DYE)
-      m.setDisplayName(ChatColor.LIGHT_PURPLE + game.id)
-      m.setLore(java.util.Arrays.asList(
-        ChatColor.LIGHT_PURPLE + game.title,
-        ChatColor.WHITE + "初期化中..."
-      ))
-    } else {
-      i.setType(Material.GRAY_DYE)
-      m.setDisplayName(ChatColor.GRAY + game.id)
-      m.setLore(java.util.Arrays.asList(
-        ChatColor.LIGHT_PURPLE + game.title,
-        ChatColor.RED + "クリックして部屋を作成します"
-      ))
+    game.state match {
+      case GameState.PLAY | GameState.WAIT | GameState.READY =>
+        i.setType(Material.LIME_DYE)
+        i.setAmount(if(game.members.isEmpty) 1 else game.members.size)
+        m.setDisplayName(ChatColor.GREEN + game.id)
+        m.setLore(java.util.Arrays.asList(
+          ChatColor.LIGHT_PURPLE + game.title,
+          ChatColor.GREEN + game.mapInfo.mapName,
+          ChatColor.GREEN + (if(game.state == GameState.PLAY) "試合中！" else if (game.state == GameState.WAIT) s"${game.members.size} / ${game.maxMember} 待機中！" else s"${game.members.size} / ${game.maxMember} 準備中！"),
+        ))
+      case GameState.PLAY2 =>
+        i.setType(Material.GRAY_DYE)
+        m.setDisplayName(ChatColor.GRAY + game.id)
+        m.setLore(java.util.Arrays.asList(
+          ChatColor.LIGHT_PURPLE + game.title,
+          ChatColor.GREEN + game.mapInfo.mapName,
+          ChatColor.GREEN + (if(game.state == GameState.PLAY) "試合中！" else if (game.state == GameState.WAIT) s"${game.members.size} / ${game.maxMember} 待機中！" else s"${game.members.size} / ${game.maxMember} 準備中！"),
+          ChatColor.RED + "もうすぐ試合が終わるので参加できません！"
+        ))
+      case GameState.INIT =>
+        i.setType(Material.PINK_DYE)
+        m.setDisplayName(ChatColor.LIGHT_PURPLE + game.id)
+        m.setLore(java.util.Arrays.asList(
+          ChatColor.LIGHT_PURPLE + game.title,
+          ChatColor.WHITE + "初期化中..."
+        ))
+      case GameState.ERROR =>
+        i.setType(Material.PINK_DYE)
+        m.setDisplayName(ChatColor.LIGHT_PURPLE + game.id)
+        m.setLore(java.util.Arrays.asList(
+          ChatColor.LIGHT_PURPLE + game.title,
+          ChatColor.RED + "マップの読込に失敗しました"
+        ))
+      case GameState.END =>
+        i.setType(Material.GRAY_DYE)
+        m.setDisplayName(ChatColor.GRAY + game.id)
+        m.setLore(java.util.Arrays.asList(
+          ChatColor.LIGHT_PURPLE + game.title,
+          ChatColor.YELLOW + "試合が終了しました"
+        ))
+      case _ =>
+        i.setType(Material.GRAY_DYE)
+        m.setDisplayName(ChatColor.GRAY + game.id)
+        m.setLore(java.util.Arrays.asList(
+          ChatColor.LIGHT_PURPLE + game.title,
+          ChatColor.RED + "クリックして部屋を作成します"
+        ))
     }
     i.setItemMeta(m)
     i
@@ -71,43 +95,5 @@ object GameUI {
       slot += 9
     })
     player.openInventory(inv)
-  }
-
-  /**
-   * 統計インベントリを開く
-   *
-   * @since v1.4.1
-   * @param player Player
-   * @param target Target
-   */
-  def openStatsUI(player: Player, target: Player): Unit = {
-    val inv = Bukkit.createInventory(null, 18, s"${target.getName}'s stats'")
-    val wp = getWPlayer(target)
-    val tdmIcon = new ItemStack(Material.IRON_SWORD)
-    val tdmIconMeta = tdmIcon.getItemMeta
-    tdmIconMeta.setDisplayName(ChatColor.YELLOW + "TDM")
-    tdmIconMeta.setLore(java.util.Arrays.asList(
-      ChatColor.GRAY + "プレイした記録: " + -1,
-      ChatColor.GRAY + "勝利した回数: " + -1,
-      ChatColor.GRAY + "敵を倒した回数: " + -1,
-      ChatColor.GRAY + "死んだ回数: " + -1,
-      ChatColor.GRAY + "与えたダメージ: " + -1,
-      ChatColor.GRAY + "*",
-      ChatColor.GRAY + "占領に成功した回数: " + -1,
-      ChatColor.GRAY + "Kill MVPを取得した回数: " + -1,
-      ChatColor.GRAY + "Damage MVPを取得した回数: " * -1
-    ))
-    tdmIconMeta.setUnbreakable(true)
-    tdmIconMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_UNBREAKABLE)
-    tdmIcon.setItemMeta(tdmIconMeta)
-
-    val tacticsIcon = new ItemStack(Material.REDSTONE, 1)
-    val tacticsIconMeta = tacticsIcon.getItemMeta
-    tacticsIconMeta.setDisplayName(ChatColor.DARK_AQUA + "Tactics")
-    tacticsIconMeta.setLore(java.util.Arrays.asList(
-      "",
-      "",
-      ""
-    ))
   }
 }
