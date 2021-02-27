c2c4e5c6d2b9542e0451de9eb8726d413c1a7428
PokkeDoll <pokkedoll@gmail.com>
2021-02-28 00:40:25 +0900

    update: new rank table

--- src/main/scala/hm/moe/pokkedoll/warscore/utils/RankManager.scala
+++ src/main/scala/hm/moe/pokkedoll/warscore/utils/RankManager.scala
@@ -3,8 +3,7 @@ package hm.moe.pokkedoll.warscore.utils
 import hm.moe.pokkedoll.warscore.{WPlayer, WarsCore}
 import net.md_5.bungee.api.ChatColor
 import org.bukkit.Sound
-import org.bukkit.scheduler.{BukkitRunnable, BukkitTask}
-import org.bukkit.scoreboard.DisplaySlot
+import org.bukkit.scheduler.BukkitRunnable
 
 /**
  * ランクを大雑把に管理するオブジェクト
@@ -23,7 +22,10 @@ object RankManager {
    * @param rank ランク
    * @return
    */
+  @Deprecated
   def getNextExp(rank: Int): Int = (100 * Math.pow(1.05, rank)).toInt
+
+  val nextExp: Int => Int = (rank: Int) => (if(67 > rank) 100 * Math.pow(1.05, rank) else 50000).toInt
 
   def giveExp(wp: WPlayer, exp: Int): Unit = {
     wp.exp += exp
